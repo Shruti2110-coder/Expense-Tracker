dbacc74264e41062814e7c4ae4e52fbd1e53505f
shruti <shruti@Vedants-MacBook-Air.local>
2026-01-14 11:17:40 +0530

    Changed backend port to 5000

--- backend/index.js
+++ backend/index.js
@@ -24,5 +24,5 @@ mongoose.connect(`${process.env.MONGO_URL}`)
   .then(()=>console.log('MongoDB connected'))
   .catch(err=>console.log(err));
 
-const PORT = 3000;
+const PORT = 5000;
 app.listen(PORT, ()=>console.log(`Server running on ${PORT}`));
